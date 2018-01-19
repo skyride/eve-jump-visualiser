d3b5cb3ac2330d1de9b4639b96e12aeadeb39c9d
Adam Findlay <adam.findlay@mercurytide.co.uk>
2018-01-19 16:03:55 +0000

    Changed name of jumps model

--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from sde.models import System
 
 
-class Jumps(models.Model):
+class JumpRecords(models.Model):
     date = models.DateTimeField(auto_now_add=True, db_index=True)
     system = models.ForeignKey(System)
     jumps = models.IntegerField(default=0)
