d362d000b874117bc64102d92f89c747310c6700
Adam Findlay <adam.findlay@mercurytide.co.uk>
2018-01-19 15:47:57 +0000

    Created jumps model

--- core/models.py
+++ core/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+from sde.models import System
+
+
+class Jumps(models.Model):
+    date = models.DateTimeField(auto_now_add=True, db_index=True)
+    system = models.ForeignKey(System)
+    jumps = models.IntegerField(default=0)
