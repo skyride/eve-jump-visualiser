b70212cee4d656abf939245ad9ba6c192662860e
Adam Findlay <adam.findlay@mercurytide.co.uk>
2018-01-19 16:14:34 +0000

    Added updatejumps command

---
+++ core/management/commands/updatejumps.py
@@ -0,0 +1,42 @@
+import requests
+
+from django.core.management.base import BaseCommand
+
+from sde.models import System
+from core.models import JumpsRecord
+
+
+class Command(BaseCommand):
+    help = "Scrapes jump data from the API"
+
+    def handle(self, *args, **options):
+        # Get data from API
+        results = requests.get("https://esi.tech.ccp.is/v1/universe/system_jumps/?datasource=tranquility")
+        results = results.json()
+
+        # Convert to dictionary
+        jumps = {}
+        for jump in results:
+            jumps[jump['system_id']] = jump['ship_jumps']
+
+        # Build db objects
+        db_jumps = []
+        total_jumps = 0
+        for system in System.objects.filter(id__lt=31000000).all():
+            jump = JumpsRecord(
+                system=system
+            )
+            if system.id in jumps:
+                jump.jumps = jumps[system.id]
+
+            total_jumps = total_jumps + jump.jumps
+            db_jumps.append(jump)
+
+        # Bulk create the objects
+        JumpsRecord.objects.bulk_create(db_jumps)
+        print (
+            "Data for %s systems added, %s total jumps" % (
+                len(results),
+                total_jumps
+            )
+        )
--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from sde.models import System
 
 
-class JumpRecords(models.Model):
+class JumpsRecord(models.Model):
     date = models.DateTimeField(auto_now_add=True, db_index=True)
     system = models.ForeignKey(System)
     jumps = models.IntegerField(default=0)
