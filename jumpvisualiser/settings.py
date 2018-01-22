8e24477acfb35e60a3bc8d9a825b0204fc45f8be
Adam Findlay <skylinerspeeder@gmail.com>
2018-01-22 18:57:10 +0100

    Switched to MySQL

--- jumpvisualiser/settings.py
+++ jumpvisualiser/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'jumpvisualiser.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'default.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jumps',
+	'USER': 'jumps',
+	'PASSWORD': 'YRlifR9w8g',
     },
     'sde': {
         'ENGINE': 'django.db.backends.sqlite3',
